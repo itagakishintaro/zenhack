9125890e5261479fbfce63d140fe4577bd016ca8
itagaki <itagaki.shintaro@gmail.com>
2015-07-04 17:46:07 +0900

    チャートの暫定版完成

--- js/chart.js
+++ js/chart.js
@@ -1,20 +1,26 @@
 'use strict';
 
+var LEVEL = ['負けた', '妥協した', '勝った'];
+
 $(function() {
 	drawLine();
-    drawPie(1);
+    chartData.forEach(function(v, i){
+        if(i > 0){
+            drawPie(i);
+        }
+    });
 });
 
 var chartData = [
     ['x', '2015-06-08', '2015-06-09', '2015-06-10', '2015-06-11', '2015-06-12', '2015-06-13', '2015-06-14'],
-    ['戒律1', 2, 5, 3, 1, 2, 0, 5],
-    ['戒律2', 0, 5, 3, 7, 5, 8, 4],
-    ['戒律3', 0, 3, 2, 0, 1, 4, 2],
-    ['戒律4', 0, 3, 1, 0, 3, 4, 2],
-    ['戒律5', 0, 3, 2, 0, 1, 2, 3],
-    ['戒律6', 0, 3, 5, 0, 1, 4, 2],
-    ['戒律7', 0, 3, 2, 0, 3, 2, 4],
-    ['戒律8', 0, 3, 2, 0, 1, 4, 2]
+    ['戒律1', 2, 0, 2, 1, 2, 0, 0],
+    ['戒律2', 0, 0, 2, 0, 0, 0, 2],
+    ['戒律3', 0, 2, 2, 0, 1, 2, 2],
+    ['戒律4', 0, 2, 1, 0, 2, 2, 2],
+    ['戒律5', 0, 2, 2, 0, 1, 2, 2],
+    ['戒律6', 0, 2, 0, 0, 1, 2, 2],
+    ['戒律7', 0, 2, 2, 0, 2, 2, 2],
+    ['戒律8', 0, 2, 2, 0, 1, 2, 2]
 ];
 
 function drawLine() {
@@ -41,16 +47,32 @@ function drawLine() {
 
 function drawPie(i) {
     var chart = c3.generate({
-        bindto: '#pie1',
+        bindto: '#pie' + i,
+        size: {
+          width: 150
+        },
         data: {
-            columns: [
-                ['data1', 30],
-                ['data2', 120],
-            ],
+            columns: createPieData(i),
             type : 'pie',
             onclick: function (d, i) { console.log("onclick", d, i); },
             onmouseover: function (d, i) { console.log("onmouseover", d, i); },
             onmouseout: function (d, i) { console.log("onmouseout", d, i); }
         }
     });
+}
+
+function createPieData(pieId){
+    var org = chartData[pieId];
+    var pieData = [];
+    LEVEL.forEach(function(level, i){
+        var data = [ level ];
+        data.push(
+            org.filter(function(v){
+                return v === i;
+            }).length
+        );
+        console.log(data);
+        pieData.push(data);
+    });
+    return pieData;
 }
