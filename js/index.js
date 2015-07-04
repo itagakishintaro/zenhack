6dd37c1346d903943f2a6cfaaded1a1f3da2a7b5
itagaki <itagaki.shintaro@gmail.com>
2015-07-05 07:26:32 +0900

    アニメーションを追加

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 'use strict';
 
 $('#yes').on('click', function(){
-	
+	change();
 });
+
+function change() {
+	$('img.chara').addClass('animated rotateOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+		$('img.chara').removeClass('animated rotateOut');
+		$('img.chara').prop('src','images/sad.jpg');
+		$('img.chara').addClass('animated zoomIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+			$('img.chara').removeClass('animated zoomIn');
+		});
+	});
+	
+
+	
+	// setTimeout(function(){
+	// 	$('img.chara').prop('src','images/sad.jpg');
+	// } , 500);
+	
+	// $('img.chara').stop().animate({ opacity:0, top:-100 }, 300, 'easeInBack', function() {
+	// 	$(this).prop('src','images/sad.jpg').delay(500).animate({ opacity:1, top: 300 }, 300, 'easeOutBounce');
+	// 	return false;	
+	// });
+}
