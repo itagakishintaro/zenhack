642ac12f6d8bafcae7575b01684ea825a60adbb5
idohiro <idohiro05092gmail.com>
2015-07-05 07:38:56 +0900

    吹き出しのコメント変更処理を追加

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 $('#yes').on('click', function(){
-	
+	$(".box").text();
+	$(".box").text("いいね")
+});
+
+$('#no').on('click', function(){
+	$(".box").text();
+	$(".box").text("戒を破らないでね")
 });
