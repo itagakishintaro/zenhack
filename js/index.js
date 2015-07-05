3f5145c8aeb667df918d4cd7eae0b7f762afa9bf
Masayuki Sato <mijabi@gmail.com>
2015-07-05 12:41:07 +0900

    いろいろ修正

--- js/index.js
+++ js/index.js
@@ -2,19 +2,29 @@
 
 $('#yes').on('click', function(){
 	$(".box").text();
-	$(".box").text("いいね");
-	change();
+	$(".box").text("いいね！");
+	change('images/kai--happy.png');
 });
 
 $('#no').on('click', function(){
 	$(".box").text();
 	$(".box").text("戒を破らないでね");
+	change('images/kai--sad.png');
 });
 
-function change() {
+function change(yourSrc) {
 	$('img.chara').addClass('animated rotateOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 		$('img.chara').removeClass('animated rotateOut');
-		$('img.chara').prop('src','images/kai--sad.png');
+		$('img.chara').prop('src',yourSrc);
+			if(yourSrc == 'images/kai--happy.png') {
+				console.log('hoge')
+				$('img.chara').css(
+					{
+						height: '187',
+						marginTop: '-30px'
+					}
+				);
+			}
 		$('img.chara').addClass('animated zoomIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 			$('img.chara').removeClass('animated zoomIn');
 		});
